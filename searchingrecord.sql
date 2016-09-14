6ae3676267463c1fab166b7e4181de25ae81dcab
Shiddiq <shiddiqitt@gmail.com>
2016-09-14 14:57:53 +0800

    Update searchingrecord.sql

--- searchingrecord.sql
+++ searchingrecord.sql
@@ -1,6 +1,7 @@
-select case  
+		select 
+			case  
 			when exists (select 1 from fuego_part_assigrole  
-						where upper(fuego_id)='L0452496') 
+					where upper(fuego_id)='L0452496') 
 			then 'Y'  else 'N' 
 			end as rec_exists 
-from dual
+		from dual
